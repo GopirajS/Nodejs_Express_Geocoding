bdb0b3ca91df4ee54d75b04edffd10b31e9f1d91
Gopi Raj <gopiraj@elitoinnovations.com>
2026-01-31 18:24:58 +0530

    Cors issue updated

--- src/app.js
+++ src/app.js
@@ -12,11 +12,10 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(morgan('dev'));
 
-app.options('*', cors());
 
 app.use(
   cors({
-    origin: '*',
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://127.0.0.1:5174'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
