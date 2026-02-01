cd9de601cd334bf37ba52024ad128ddb558aed25
Gopi Raj <gopiraj@elitoinnovations.com>
2026-02-01 07:53:52 +0530

    Glopal access

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'));
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://127.0.0.1:5174','https://gopi-billing-systems.netlify.app'],
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
